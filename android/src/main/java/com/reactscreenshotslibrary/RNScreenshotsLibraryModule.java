2244005f00161a27059036ee28aef4e6367d8399
Коробов Дмитрий <dkorobov@kassa24.kz>
2020-02-10 11:51:55 +0600

    changes

--- android/src/main/java/com/reactscreenshotslibrary/RNScreenshotsLibraryModule.java
+++ android/src/main/java/com/reactscreenshotslibrary/RNScreenshotsLibraryModule.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package com.reactscreenshotslibrary;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
